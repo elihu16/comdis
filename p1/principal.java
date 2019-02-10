27bf73ae3d5b233b581be4697496d8152cafbae2
elihu <elihu1996@hotmail.com>
2019-02-10 14:35:08 -0600

    cambie los nombres

--- p1/principal.java
+++ p1/principal.java
@@ -1,18 +1,18 @@
 public class principal {
     public static void main(String[] args) {
-        talumno Deniz = new talumno();
-        Deniz.SetNombre("Deniz Aniceto Marin");
-        Deniz.SetEdad(15);
-        Deniz.SetGenero('f');
+        talumno elihu = new talumno();
+        elihu.SetNombre("elihu Baltazar Diaz");
+        elihu.SetEdad(22);
+        elihu.SetGenero('m');
         
-        Deniz.setCuatrimestre(8);
-        Deniz.setMatricula("201538499");
-        Deniz.setSeccion(103);
+        elihu.setCuatrimestre(8);
+        elihu.setMatricula("201535188");
+        elihu.setSeccion(103);
         
-        System.out.println(Deniz.toString());
+        System.out.println(elihu.toString());
         
-        /*System.out.println("nombre: "+ Deniz.GetNombre());
-        System.out.println("Edad: "+ Deniz.GetEdad());
-        System.out.println("Genero: " + Deniz.GetGenero());*/
+        /*System.out.println("nombre: "+ elihu.GetNombre());
+        System.out.println("Edad: "+ elihu.GetEdad());
+        System.out.println("Genero: " + elihu.GetGenero());*/
     }
 }//fin  de la clase principal
