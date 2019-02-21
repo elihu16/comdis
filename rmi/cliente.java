0683b5d2ba1672b8c4c9c17bf41da58ab9ea8b14
root <root@deb>
2019-02-20 19:32:40 -0500

    rmi terminado

---
+++ rmi/cliente.java
@@ -0,0 +1,14 @@
+import java.rmi.Naming;
+
+public class cliente{
+    public static void main(String []args){
+        try{
+            System.setProperty("java.security.policy","rmi.policy");
+            System.setSecurityManager(new SecurityManager());
+            iobjetoremoto objrem = (iobjetoremoto) Naming.lookup("rmi://localhost:2320/SistemasDistribuidos");
+            //invocacion a los metodos remotos
+            objrem.Saludo("hola mundo desde la FCC");
+        }
+        catch(Exception e){e.printStackTrace();}
+    }//fin del main
+}//fin de la clase cliente
--- rmi/iobjetoremoto.java
+++ rmi/iobjetoremoto.java
@@ -1,8 +1,8 @@
-import java.rmi.RemoteException
-import java.rm1.RemoteException
+import java.rmi.RemoteException;
+import java.rmi.Remote;
 
 public interface iobjetoremoto extends Remote{
     //prototipos de los metodos a utilizar remotamente
-    public void Saludo(String mensaje) throws RemoteExceprion;
-    
+    public void Saludo(String mensaje) throws RemoteException;
+
 }//fin de la interfaz iobjetoremoto
