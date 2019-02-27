df10ff5bcfe842d774dac848b0a90ac5cba69e5c
elihulapwin <elihu1996@hotmail.com>
2019-02-27 17:24:11 -0600

    plantilla

--- rmiP3/cliente.java
+++ rmiP3/cliente.java
@@ -6,14 +6,8 @@ public class cliente{
             System.setProperty("java.security.policy","rmi.policy");
             System.setSecurityManager(new SecurityManager());
             iobjetoremoto objrem = (iobjetoremoto) Naming.lookup("rmi://localhost:2320/SistemasDistribuidos");
-            
             //invocacion a los metodos remotos
-            double a = 5;
-            double b = 0;
-            System.out.println("Suma:"+ objrem.Suma(a, b));
-            System.out.println("Resta: "+ objrem.Resta(a, b));
-            System.out.println("Multiplicacion: "+ objrem.Multiplicacion(a, b));
-            System.out.println("Division: "+ objrem.Division(a, b));
+            objrem.Saludo("hola mundo desde la FCC");
         }
         catch(Exception e){e.printStackTrace();}
     }//fin del main
