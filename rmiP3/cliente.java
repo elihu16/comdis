51a882c5b00eb114140264b91912b77faba3ab19
root <root@deb>
2019-02-27 19:15:59 -0500

    practica_de_cajero

--- rmiP3/cliente.java
+++ rmiP3/cliente.java
@@ -8,8 +8,10 @@ public class cliente{
             iobjetoremoto objrem = (iobjetoremoto) Naming.lookup("rmi://localhost:2320/SistemasDistribuidos");
             //invocacion a los metodos remotos
             Scanner lectura = new Scanner(System.in);
+            Scanner leer = new Scanner(System.in);
             int cliente;
             double saldo;
+            int opc;
             System.out.println("Cajero Automatico Elihu\n");
             System.out.print("Dame el numero de cliente");
             cliente = lectura.nextInt();
@@ -20,7 +22,27 @@ public class cliente{
                 System.out.println("3-Depositar");
                 System.out.print("ingresa tu opcion");
                 opc = lectura.nextInt();
-                
+
+                switch(opc){
+                    case 1:
+                        saldo = objrem.DevolverSaldo(cliente);
+                        System.out.println("Saldo disponible: "+ saldo);
+                        break;
+                    case 2:
+                        System.out.print("Dame la cantidad a retirar: ");
+                        saldo = leer.nextDouble();
+                        objrem.CambiarSaldo(cliente, saldo*(-1));
+                        System.out.print("Tu saldo actual es: " + objrem.DevolverSaldo(cliente));
+                        break;
+                    case 3:
+                        System.out.print("Dame la cantidad a depositar: ");
+                        saldo = leer.nextDouble();
+                        objrem.CambiarSaldo(cliente, saldo);
+                        System.out.print("Tu saldo actual es: " + objrem.DevolverSaldo(cliente));
+                        break;
+                    default:
+                        System.out.println("opcion incorrecta");
+                }
             }
             else{
                 System.out.println("cliente inexistente");
