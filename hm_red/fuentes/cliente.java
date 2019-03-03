3d173b706176ab513e2c1e1ed9334c6ba15c17de
elihulapwin <elihu1996@hotmail.com>
2019-03-03 08:41:42 -0600

    mod

--- hm_red/fuentes/cliente.java
+++ hm_red/fuentes/cliente.java
@@ -1,13 +1,22 @@
 import java.rmi.Naming;
-
+import java.util.Scanner;
 public class cliente{
     public static void main(String []args){
         try{
             System.setProperty("java.security.policy","rmi.policy");
             System.setSecurityManager(new SecurityManager());
-            iobjetoremoto objrem = (iobjetoremoto) Naming.lookup("rmi://172.31.9.221:2320/SistemasDistribuidos");
+            /*iobjetoremoto objrem = (iobjetoremoto) Naming.lookup("rmi://172.31.9.221:2320/SistemasDistribuidos");
+            */
+            iobjetoremoto objrem = (iobjetoremoto) Naming.lookup("rmi://localhost:2320/SistemasDistribuidos");
+            
+            
             //invocacion a los metodos remotos
-            objrem.Saludo("hola mundo desde la FCC");
+            java.util.Scanner lectura = new Scanner(System.in);
+            String cadena;
+
+            System.out.print("Escribe un saludo");
+            cadena = lectura.nextLine();
+            objrem.Saludo(cadena);
         }
         catch(Exception e){e.printStackTrace();}
     }//fin del main
